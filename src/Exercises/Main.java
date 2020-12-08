2197f98620922f149ce3e83114bed2f48c0b3447
Krzysztof Chęciński <checinski.krzysztof@gmail.com>
2020-12-08 23:36:10 +0100

    Solving the exercise 1

--- src/Exercises/Main.java
+++ src/Exercises/Main.java
@@ -1,7 +1,16 @@
 package Exercises;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("Insert integer");
+        Scanner in = new Scanner(System.in);
+        int variable = in.nextInt();
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(variable + " X " + i + " = " + (variable * i));
+        }
 
     }
 }
